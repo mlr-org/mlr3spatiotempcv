39c3b5bb20526a6c3565afb25b2a2bb335b4018a
pat-s <patrick.schratz@gmail.com>
2019-08-04 22:51:10 +0200

    add WIP script

---
+++ inst/spatial-plots.R
@@ -0,0 +1,22 @@
+library(sf)
+library(ggplot2)
+
+set.seed(123)
+
+task = mlr_tasks$get("ecuador")
+resampling_cv = mlr_resamplings$get("spcv-kmeans", param_vals = list(folds = 3))
+resampling_cv$instantiate(task)
+
+coords = task$coordinates()
+coords$row_id = sort(1:task$nrow)
+
+coords_resamp = merge(coords, resampling_cv$instance, by = "row_id")
+
+sf_df = st_as_sf(coords_resamp, coords = c("x", "y"))
+sf_df$fold = as.factor(as.character(sf_df$fold))
+
+ggplot() +
+  geom_sf(data = sf_df, aes(color = fold)) +
+  scale_color_viridis_d()
+
+
