e00a560c669cac43b3a005d3d788f2cab002b659
be-marc <marcbecker@posteo.de>
2019-08-13 00:14:27 +0200

    Add examples

---
+++ inst/mlr3spatiotemporal_test.R
@@ -0,0 +1,199 @@
+library(mlr3)
+library(mlr3spatiotemporal)
+library(blockCV)
+library(sf)
+library(R6)
+library(paradox)
+library(data.table)
+
+
+# ResamplingSpCVBlock - No paramter
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task)
+
+# ResamplingSpCVBlock - range + checkerboard
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4, range=500, selection = "checkerboard")
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task)
+
+# ResamplingSpCVBlock - rows + cols + systematic
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4, rows = 10, cols = 10, selection = "systematic")
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task)
+
+# ResamplingSpCVBlock - rows + cols + range
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4, rows = 10, cols = 10, range=1000)
+rcv$instantiate(task)
+
+
+# ResamplingSpCVEnv - All features
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVEnv$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task)
+
+# ResamplingSpCVEnv - feature selection
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVEnv$new()
+rcv$param_set$values <- list(folds = 4, features=c("dem"))
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task)
+
+# ResamplingSpCVEnv - Wrong feature
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVEnv$new()
+rcv$param_set$values <- list(folds = 4, features=c("abc"))
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task)
+
+
+# ResampleSpCVKmeans - No parameter
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVKmeans$new()
+rcv$instantiate(task)
+
+resample(task, learner, rcv)
+
+plot(rcv, task)
+
+# ResampleSpCVKmeans - folds
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVKmeans$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+plot(rcv, task)
+
+
+# ResampleSpCVBuffer - range
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBuffer$new()
+rcv$param_set$values <- list(range = 1000)
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task, 1)
+
+# ResampleSpCVBuffer - No parameter
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBuffer$new()
+rcv$instantiate(task)
+
+resampling = resample(task, learner, rcv)
+
+plot(rcv, task, 1)
+
+
+# Vizualiation - Multiplot all folds
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+plot(rcv, task, fold_id=c(1,2,3,4))
+
+# Vizualiation - Multiplot
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+plot(rcv, task, fold_id=c(1,2,3))
+
+# Vizualiation - Too many folds
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+plot(rcv, task, fold_id=c(1,2,3,4,5))
+
+# Vizualiation - One fold
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+plot(rcv, task, fold_id=1)
+
+# Vizualiation - All folds one plot
+
+task <- mlr3::mlr_tasks$get("ecuador")
+learner = mlr_learners$get("classif.rpart")
+
+rcv <- ResamplingSpCVBlock$new()
+rcv$param_set$values <- list(folds = 4)
+rcv$instantiate(task)
+
+plot(rcv, task)
+
