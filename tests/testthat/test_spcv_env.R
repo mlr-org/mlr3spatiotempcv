99b04b26dee9e49be9767cca2413a3f40dd70640
pat-s <patrick.schratz@gmail.com>
2019-09-03 16:58:20 +0200

    fix spcv-env tst

--- tests/testthat/test_spcv_env.R
+++ tests/testthat/test_spcv_env.R
@@ -6,5 +6,5 @@ task = mlr_tasks$get("diplodia")
 test_that("cols and rows are ignored when range is given", {
   rsp = mlr_resamplings$get("spcv-env")
   rsp$param_set$values = list(features = "lithology", folds = 4)
-  expect_warning(rsp$instantiate(task), "Cols and rows are ignored. Range is used to generated blocks")
+  expect_error(rsp$instantiate(task), "requires numeric features for clustering")
 })
