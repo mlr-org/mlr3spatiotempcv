2300ded3fe7e2ab3dc456631ec8cb2e2302cb0de
pat-s <patrick.schratz@gmail.com>
2019-09-03 12:06:25 +0200

    make spcv tests generic

---
+++ tests/testthat/test_mlr_spcv_generic.R
@@ -0,0 +1,54 @@
+context("mlr_resampling_spcv-coords")
+
+# init objects -----------------------------------------------------------------
+
+spcv_rsp = mlr_resamplings$mget(
+  as.data.table(mlr_resamplings)[map_lgl(key, stringr::str_detect, pattern = "spcv"), key]
+)
+# because 'spcv-buffer' has no fold argument, we need to create an extra group
+# for some tests
+spcv_rsp_no_buffer = spcv_rsp
+spcv_rsp_no_buffer$`spcv-buffer` = NULL
+
+task = mlr_tasks$get("ecuador")
+
+# create custom data for grouping variable
+b <- as.data.table(readRDS(system.file("extdata", "ecuador.rda",
+  package = "mlr3spatiotemporal"
+)))
+
+# add grouping variable
+data <- insert_named(b[1:150, ], list(grp = rep_len(letters[1:10], 150)))
+task_grp <- TaskClassifST$new("ecuador-grp", as_data_backend(data), target = "slides",
+  coordinates = c("x", "y"))
+task_grp$set_col_role("grp", "groups")
+
+# run tests --------------------------------------------------------------------
+
+test_that("spcv has no duplicated ids", {
+  for (i in spcv_rsp) {
+    expect_identical(i$duplicated_ids, FALSE)
+  }
+})
+
+test_that("stratification", {
+  for (i in spcv_rsp_no_buffer) {
+    i$param_set$values <- list(folds = 5, stratify = TRUE)
+    expect_error(i$instantiate(task))
+  }
+  spcv_rsp$`spcv-buffer`$param_set$values = list(stratify = TRUE, range = 100)
+  expect_error(spcv_rsp$`spcv-buffer`$instantiate(task))
+})
+
+# restore initial objects without modification
+spcv_rsp = mlr_resamplings$mget(
+  as.data.table(mlr_resamplings)[map_lgl(key, stringr::str_detect, pattern = "spcv"), key]
+)
+spcv_rsp_no_buffer = spcv_rsp
+spcv_rsp_no_buffer$`spcv-buffer` = NULL
+
+test_that("grouping", {
+  for (i in spcv_rsp) {
+    expect_error(i$instantiate(task_grp))
+  }
+})
