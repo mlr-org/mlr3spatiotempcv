5348ac55af08c250d6fc12f07951fbac5605c9a5
Patrick Schratz <patrick.schratz@gmail.com>
2019-09-04 17:06:17 +0200

    Add TaskRegrST (#21)
    * add TaskRegrST
    
    * upd TaskClassifST with upstream TaskClassif changes
    
    * fix example
    
    * remove example for now

--- R/TaskRegrST.R
+++ R/TaskRegrST.R
@@ -1,42 +1,39 @@
-#' @title SpatioTemporal Classification Task
+#' @title SpatioTemporal Regression Task
 #'
 #' @import data.table
 #' @import mlr3
 #'
 #' @usage NULL
-#' @format [R6::R6Class] object inheriting from
-#'   [Task]/[TaskSupervised]/[TaskClassif].
+#' @format [R6::R6Class] object inheriting from [Task]/[TaskSupervised]/[TaskRegr].
 #'
-#' @description This task specializes [Task] and [TaskSupervised] for
-#' spatio-temporal classification problems. The target column is assumed to be a
-#' factor. The `task_type` is set to `"classif"` and `"spatiotemporal"`.
+#' @description
+#' This task specializes [Task] and [TaskSupervised] for spatio-temporal regression problems.
+#' The target column is assumed to be a factor.
+#' The `task_type` is set to `"classif"` and `"spatiotemporal"`.
 #'
-#' A spatial example task is available via `mlr_tasks$get("ecuador")`. During
-#' initialization, coordinates need to be passed. By default, coordinates are
-#' not used as features. This can be changed by setting `coords_as_features =
-#' TRUE`.
+#' A spatial example task is available via `mlr_tasks$get("ecuador")`.
+#' During initialization, coordinates need to be passed.
+#' By default, coordinates are not used as features.
+#' This can be changed by setting `coords_as_features = TRUE`.
 #'
 #' @section Construction:
 #' ```
-#' t = TaskClassifST$new(id, backend, target, coordinates, positive = NULL,
+#' t = TaskRegrST$new(id, backend, target, coordinates,
 #'   coords_as_features = FALSE)
 #' ```
 #'
 #' * `id` :: `character(1)`\cr
 #'   Identifier for the task.
 #'
-#' * `backend` :: [DataBackend]\cr Either a [DataBackend], or any object which
-#' is convertible to a DataBackend with `as_data_backend()`. E.g., a
-#' `data.frame()` will be converted to a [DataBackendDataTable].
+#' * `backend` :: [DataBackend]\cr
+#'   Either a [DataBackend], or any object which is convertible to a DataBackend with `as_data_backend()`.
+#'   E.g., a `data.frame()` will be converted to a [DataBackendDataTable].
 #'
 #' * `target` :: `character(1)`\cr
 #'   Name of the target column.
 #'
 #' * `coordinates` :: [data.frame]\cr
 #'   A [data.frame] with `ncol(2)` containing longitude and latitude information.
-#'
-#' * `positive` :: `character(1)`\cr
-#'   Only for binary classification: Name of the positive class.
 #'
 #' * `coords_as_features` :: `logical(1)`\cr
 #'   Whether the coordinates should also be used as features. Default is `FALSE`.
@@ -45,65 +42,40 @@
 #'   Coordinates reference system
 #'
 #' @section Fields:
-#' All methods from [TaskSupervised] and [TaskClassif], and additionally:
+#' All methods from [TaskSupervised] and [TaskRegr], and additionally:
 #'
 #' * `coordinates` :: [data.frame]\cr
 #'   Stores the coordinates passed during construction.
 #'
 #' @section Methods:
-#' See [TaskSupervised] and [TaskClassif].
+#' See [TaskSupervised] and [TaskRegr].
 #'
 #' @family Task
-#' @seealso Example spatial classification task: [`ecuador`][mlr_tasks_ecuador].
+#' @seealso Example spatial regression task: [`ecuador`][mlr_tasks_ecuador].
 #' @export
 #' @examples
-#' data = data.table::as.data.table(readRDS(system.file("extdata", "ecuador.rda",
-#'   package = "mlr3spatiotemporal")))
-#' task = TaskClassifST$new("ecuador", backend = data, target = "slides",
-#'   positive = "TRUE", coordinates = c("x", "y"))
-#'
-#' task$task_type
-#' task$formula()
-#' task$truth()
-#' task$class_names
-#' task$positive
-#' task$negative
-#' task$coordinates()
-#' task$coordinate_names
 #'
 #' # possible properties:
-#' mlr3::mlr_reflections$task_properties$classif
-TaskClassifST = R6::R6Class("TaskClassifST",
-  inherit = TaskClassif,
+#' mlr3::mlr_reflections$task_properties$regr
+TaskRegrST <- R6::R6Class("TaskRegrST",
+  inherit = TaskRegr,
   public = list(
     crs = NULL,
 
-    initialize = function(id, backend, target, coordinates, coords_as_features = FALSE, positive = NULL, crs = NULL) {
-
+    initialize = function(id, backend, target, coordinates, coords_as_features = FALSE, crs = NULL) {
       assert_string(target)
-      super$initialize(id = id, backend = backend, target = target, positive = positive)
-
-      info = self$col_info[id == target]
-      levels = info$levels[[1L]]
-
-      if (info$type %nin% c("factor", "character")) {
-        stopf("Target column '%s' must be a factor or character", target)
-      }
-      if (length(levels) < 2L) {
-        stopf("Target column '%s' must have at least two levels", target)
-      }
-
-      self$properties = union(self$properties, if (length(levels) == 2L) "twoclass" else "multiclass")
-      if (!is.null(positive)) {
-        self$positive = positive
-      }
-
+      super$initialize(id = id, backend = backend, target = target)
       self$crs = checkmate::assert_character(crs, null.ok = TRUE)
 
       # check coordinates
       assert_names(self$backend$colnames, must.include = coordinates)
       for (coord in coordinates) {
         assert_numeric(self$data(cols = coord)[[1L]], any.missing = FALSE)
+      }
+
+      type = self$col_info[id == target]$type
+      if (type %nin% c("integer", "numeric")) {
+        stopf("Target column '%s' must be numeric", target)
       }
 
       # mark columns as coordinates
@@ -117,13 +89,13 @@ TaskClassifST = R6::R6Class("TaskClassifST",
     },
 
     truth = function(row_ids = NULL) {
-      self$data(row_ids, cols = self$target_names)[[1L]]
+      super$truth(row_ids)[[1L]]
     },
 
     coordinates = function(row_ids = NULL) {
       if (is.null(row_ids)) {
         # Return coords in task$data order
-        row_ids = self$row_ids
+        row_ids <- self$row_ids
       }
       self$backend$data(rows = row_ids, cols = self$coordinate_names)
     },
@@ -154,7 +126,6 @@ TaskClassifST = R6::R6Class("TaskClassifST",
     pmap(types, function(type, N, feats) catf(str_indent(sprintf("* %s (%i):", type, N), feats)))
   }
 
-  # nocov start
   if (length(self$col_roles$order)) {
     catf(str_indent("Order by:", self$col_roles$order))
   }
@@ -164,6 +135,5 @@ TaskClassifST = R6::R6Class("TaskClassifST",
   if ("weights" %in% self$properties) {
     catf(str_indent("Weights:", self$col_roles$weights))
   }
-  # nocov end
 
 }
