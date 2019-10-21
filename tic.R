f40808ff5f7dbd663f7044f83865fbde12a7e94d
pat-s <patrick.schratz@gmail.com>
2019-10-21 12:47:00 +0200

    fix build

--- tic.R
+++ tic.R
@@ -1,6 +1,8 @@
 do_package_checks(error_on = "error")
 
 if (ci_has_env("BUILD_PKGDOWN")) {
+  get_stage("install") %>%
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown(orphan = TRUE)
 }
 
