406fdce081338aca4d475aefcef9fc21462bc6a0
be-marc <marcbecker@posteo.de>
2020-01-28 19:37:11 +0100

    Remove old group code

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,16 +1,3 @@
-task = mlr_tasks$get("ecuador")
-
-### create custom data for grouping variable
-b = as.data.table(readRDS(system.file("extdata", "ecuador.rda",
-  package = "mlr3spatiotempcv"
-)))
-# add grouping variable
-data = insert_named(b[1:150, ], list(grp = rep_len(letters[1:10], 150)))
-task_grp = TaskClassifST$new("ecuador-grp", as_data_backend(data),
-  target = "slides",
-  coordinate_names = c("x", "y"))
-task_grp$col_roles$group = "grp"
-
 # Create 6x6 point grid with 1m distance between points
 TEST_MAKE_SP = function() {
   coordinates = expand.grid(315172:315177, 5690670:5690675)
