0c11a7cae3b1eccd07470e316955b86906c3eefe
be-marc <marcbecker@posteo.de>
2020-01-28 16:47:31 +0100

    Add features to unit test

--- tests/testthat/test_ResamplingRepeatedSpCVEnv.R
+++ tests/testthat/test_ResamplingRepeatedSpCVEnv.R
@@ -3,7 +3,7 @@ context("ResamplingRepeatedSpCVEnv")
 test_that("folds can be printed", {
   task = TEST_MAKE_TWOCLASS()
   rsp = rsmp("repeated-spcv-env")
-  rsp$param_set$values = list(folds = 3, repeats = 5)
+  rsp$param_set$values = list(folds = 3, repeats = 5, features = "p_1")
   rsp$instantiate(task)
 
   expect_equal(rsp$folds(4:8), c(4, 5, 1, 2, 3))
@@ -12,7 +12,7 @@ test_that("folds can be printed", {
 test_that("reps can be printed", {
   task = TEST_MAKE_TWOCLASS()
   rsp = rsmp("repeated-spcv-env")
-  rsp$param_set$values = list(folds = 3, repeats = 5)
+  rsp$param_set$values = list(folds = 3, repeats = 5, features = "p_1")
   rsp$instantiate(task)
 
   expect_equal(rsp$repeats(4:8), c(2, 2, 2, 3, 3))
