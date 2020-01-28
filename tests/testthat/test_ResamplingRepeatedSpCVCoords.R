0332dd3d3c491f09fed6a1dfee15cc70f303bb1e
be-marc <marcbecker@posteo.de>
2020-01-28 19:28:50 +0100

    Rewrite unit tests for ResamplingRepeatedSpCVCoords

---
+++ tests/testthat/test_ResamplingRepeatedSpCVCoords.R
@@ -0,0 +1,23 @@
+context("ResamplingRepeatedSpCVCoords")
+
+test_that("folds can be printed", {
+  rsp = rsmp("repeated-spcv-coords", folds = 3, repeats = 5)
+  rsp$instantiate(task)
+
+  expect_equal(rsp$folds(4:8), c(4, 5, 1, 2, 3))
+})
+
+test_that("reps can be printed", {
+  rsp = rsmp("repeated-spcv-coords", folds = 3, repeats = 5)
+  rsp$instantiate(task)
+
+  expect_equal(rsp$repeats(4:8), c(2, 2, 2, 3, 3))
+})
+
+test_that("resampling iterations equals folds * repeats", {
+  task = TEST_MAKE_TWOCLASS()
+  rsp = rsmp("repeated-spcv-coords", folds = 3, repeats = 2)
+  rsp$instantiate(task)
+
+  expect_equal(rsp$iters, 6)
+})
