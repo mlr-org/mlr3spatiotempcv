104a02955e6fb14711cd33d9a6faf9529c2133fd
Marc Becker <33069354+be-marc@users.noreply.github.com>
2020-02-04 21:07:43 +0100

    Unit tests (#43)
    * add/update unit tests, especially for `SpCVEnv`

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,16 +1,3 @@
-task = mlr_tasks$get("ecuador")
-
-### create custom data for grouping variable
-b = as.data.table(readRDS(system.file("extdata", "ecuador.rda",
-  package = "mlr3spatiotempcv"
-)))
-# add grouping variable
-data = insert_named(b[1:150, ], list(grp = rep_len(letters[1:10], 150)))
-task_grp = TaskClassifST$new("ecuador-grp", as_data_backend(data),
-  target = "slides",
-  coordinate_names = c("x", "y"))
-task_grp$col_roles$group = "grp"
-
 # Create 6x6 point grid with 1m distance between points
 TEST_MAKE_SP = function() {
   coordinates = expand.grid(315172:315177, 5690670:5690675)
@@ -19,7 +6,7 @@ TEST_MAKE_SP = function() {
 }
 
 # Create regression task
-TEST_MAKE_REGR = function() {
+TEST_MAKE_REGR = function(coords_as_features = FALSE) {
   data = TEST_MAKE_SP()
   data$p_1 = c(rep("A", 18), rep("B", 18))
   data$response = rnorm(36)
@@ -29,22 +16,37 @@ TEST_MAKE_REGR = function() {
     coordinate_names = c("x", "y"),
     crs = "+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs",
     target = "response",
-    coords_as_features = FALSE)
+    coords_as_features = coords_as_features)
 }
 
 # Create twoclass task
-TEST_MAKE_TWOCLASS = function() {
+TEST_MAKE_TWOCLASS = function(group = FALSE, coords_as_features = FALSE, features = "numeric") {
+
   data = TEST_MAKE_SP()
-  data$p_1 = rnorm(36)
+  if ("numeric" %in% features) {
+    data$p_1 = c(rnorm(18, 0), rnorm(18, 10))
+  }
+  if ("factor" %in% features) {
+    data$p_2 = as.factor(c(rep("lvl_1", 18), rep("lvl_2", 18)))
+  }
   data$response = as.factor(c(rep("A", 18), rep("B", 18)))
 
-  TaskClassifST$new(id = "sp_twoclass",
+  if (group) {
+    data$group = rep_len(letters[1:10], 36)
+  }
+
+  task = TaskClassifST$new(id = "sp_twoclass",
     backend = data,
     coordinate_names = c("x", "y"),
     crs = "+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs",
     target = "response",
     positive = "A",
-    coords_as_features = FALSE)
+    coords_as_features = coords_as_features)
+
+  if (group) {
+    task$col_roles$group = "group"
+  }
+  task
 }
 
 # Create multiclass task
--- tests/testthat/test_TaskRegrST.R
+++ tests/testthat/test_TaskRegrST.R
@@ -1,13 +1,13 @@
-context("TaskClassifST")
+context("TaskRegrST")
 
 test_that("coordinates can be used as features", {
-  task = TEST_MAKE_TWOCLASS(coords_as_features = TRUE)
+  task = TEST_MAKE_REGR(coords_as_features = TRUE)
 
   expect_names(c("x", "y"), subset.of = task$feature_names)
 })
 
 test_that("printing works", {
-  task = TEST_MAKE_TWOCLASS()
+  task = TEST_MAKE_REGR()
 
   expect_output(print(task))
   expect_data_table(task$coordinates())
