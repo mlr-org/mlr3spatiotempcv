45758cbcab6ba54f21d3ea8a365a7c647b95f882
pat-s <patrick.schratz@gmail.com>
2020-05-25 16:40:45 +0200

    add cookfarm regression task

---
+++ inst/extdata/cookfarm.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(sf)
+library(GSIF)
+data(cookfarm)
+set.seed(42)
+
+cookfarm_profiles = cookfarm$profiles
+
+cookfarm_mlr3 = cookfarm_profiles %>%
+  left_join(cookfarm$readings %>% select("Date", "SOURCEID"), by = "SOURCEID") %>%
+  st_as_sf(coords = c("Easting", "Northing"), crs = 26911) %>%
+  mutate(x = st_coordinates(.)[, "X"]) %>%
+  mutate(y = st_coordinates(.)[, "Y"]) %>%
+  na.omit() %>%
+  sample_n(500)
+
+saveRDS(cookfarm_mlr3, "inst/extdata/cookfarm.R")
+
+# mapview::mapview(cookfarm_mlr3)
+
+### unique timestamps
+# length(unique(cookfarm_mlr3$Date))
+# = 365 (out of 500)
+
+### unique coordinates
+# st_coordinates(cookfarm_mlr3) %>%
+#   as.data.frame() %>%
+#   group_by(X, Y) %>%
+#   unique()
+# = 41
