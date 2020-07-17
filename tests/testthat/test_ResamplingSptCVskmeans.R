fadf7d6c6a508bd97b665c54e1191597727653b1
pat-s <patrick.schratz@gmail.com>
2020-07-17 20:58:16 +0200

    add test file

---
+++ tests/testthat/test_ResamplingSptCVskmeans.R
@@ -0,0 +1,44 @@
+context("ResamplingSptCVskmeans")
+
+test_that("resampling iterations equals folds", {
+  skip_on_os("mac")
+
+  task = test_make_twoclass()
+  rsp = rsmp("spcv-cluto", folds = 2, features = "p_1")
+  rsp$instantiate(task)
+
+  expect_equal(rsp$iters, 2)
+})
+
+test_that("feature p_1 seperates the observations in two folds of equal size", {
+  set.seed(1)
+
+  skip_on_os("mac")
+
+  task = test_make_twoclass()
+  rsp = rsmp("spcv-cluto", folds = 2, features = "p_1")
+  rsp$instantiate(task)
+
+  expect_equal(rsp$test_set(1), 19:36)
+  expect_equal(rsp$train_set(1), 1:18)
+  expect_equal(rsp$test_set(2), 1:18)
+  expect_equal(rsp$train_set(2), 19:36)
+})
+
+test_that("non-numeric feature throws an error", {
+  skip_on_os("mac")
+
+  task = test_make_twoclass()
+  rsp = rsmp("spcv-cluto", folds = 2, features = "p_2")
+
+  expect_error(rsp$instantiate(task))
+})
+
+test_that("non-existing feature throws an error", {
+  skip_on_os("mac")
+
+  task = test_make_twoclass()
+  rsp = rsmp("spcv-cluto", folds = 2, features = "p_3")
+
+  expect_error(rsp$instantiate(task))
+})
