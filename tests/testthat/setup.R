38b84aa7f77f24afc7e4fefdc7de0be738800e1d
pat-s <patrick.schratz@gmail.com>
2020-07-28 18:57:36 +0200

    lintr

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,5 @@
 if (Sys.getenv("CI") == "true") {
   if (Sys.info()["sysname"] != "Darwin") {
-    source("https://gist.githubusercontent.com/pat-s/6430470cf817050e27d26c43c0e9be72/raw/c90480893fdd57ce990795a702aada41382e03a9/install-cluto.R")
+    source("https://gist.githubusercontent.com/pat-s/6430470cf817050e27d26c43c0e9be72/raw/c90480893fdd57ce990795a702aada41382e03a9/install-cluto.R") # nolint
   }
 }
