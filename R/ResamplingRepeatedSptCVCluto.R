8091196f159cc59cc322a5ef4033d4110a932521
pat-s <patrick.schratz@gmail.com>
2020-07-30 16:59:21 +0200

    outsource to helper fun

--- R/ResamplingRepeatedSptCVCluto.R
+++ R/ResamplingRepeatedSptCVCluto.R
@@ -183,39 +183,7 @@ ResamplingRepeatedSptCVCluto = R6Class("ResamplingRepeatedSptCVCluto",
         control_cluto = sprintf("-clmethod='%s''%s'", clmethod, cluto_parameters)
       }
 
-      if (!Sys.getenv("CLUTO_PATH") == "") {
-        vcluster_loc = Sys.getenv("CLUTO_PATH")
-      } else {
-        cli::cli_alert_info("CLUTO executable path not set via env var
-          {.var CLUTO_PATH}.
-          Trying to find it in {.pkg mlr3spatiotempcv}.", wrap = TRUE)
-
-        vcluster_loc = switch(Sys.info()[["sysname"]],
-          "Windows" = {
-            if (!file.exists(system.file("vcluster.exe",
-              package = "mlr3spatiotempcv"))) {
-              cli::cli_alert_danger("{.file vcluster.exe} not found. Please
-              install CLUTO first. See {.code ?ResamplingSptCVCluto} for
-              instructions.", wrap = TRUE)
-              stop("CLUTO executable not found.")
-            }
-            system.file("vcluster.exe",
-              package = "mlr3spatiotempcv")
-          },
-          "Linux" = {
-            if (!file.exists(system.file("vcluster",
-              package = "mlr3spatiotempcv"))) {
-              cli::cli_alert_danger("{.file vcluster} not found. Please install
-              CLUTO first. See {.code ?ResamplingSptCVCluto} for
-              instructions.", wrap = TRUE)
-              stop("CLUTO executable not found.")
-            }
-            system.file("vcluster",
-              package = "mlr3spatiotempcv")
-          },
-          "Darwin" = stop("macOS is not supported by CLUTO.")
-        )
-      }
+      check_cluto_path()
 
       mlr3misc::map_dtr(seq_len(pv$repeats), function(i) {
         data.table(
