f790218b5582ba9832a10b4deea388ef5f4d7dc5
pat-s <patrick.schratz@gmail.com>
2020-09-16 12:20:48 +0200

    cleanup pipelines test

--- tests/testthat/helper_autoplot.R
+++ tests/testthat/helper_autoplot.R
@@ -1,9 +1,6 @@
 # for tsk()
 loadNamespace("mlr3")
 
-# for graph learner tests
-library(mlr3pipelines)
-
 prepare_autoplot = function(resampling_type, task = NULL, ...) {
   if (is.null(task)) {
     task = test_make_twoclass()
