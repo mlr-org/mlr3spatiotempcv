1aefd212a3b0a13239ab6048e07c19402c9b477c
pat-s <patrick.schratz@gmail.com>
2020-10-21 07:44:36 +0200

    delete unneeded translate_types()

--- R/helper.R
+++ R/helper.R
@@ -1,10 +1,3 @@
-# FIXME: Remove when exported in mlr3
-translate_types = function(x) {
-  r_types = mlr_reflections$task_feature_types
-  p_types = names(mlr_reflections$task_feature_types)
-  factor(map_values(x, r_types, p_types), levels = p_types)
-}
-
 check_cluto_path = function() {
   if (!Sys.getenv("CLUTO_PATH") == "") {
     vcluster_loc = normalizePath(Sys.getenv("CLUTO_PATH"))
