9a2ff536a2654786345c8b49bc0f33218fa90076
Michel Lang <michellang@gmail.com>
2020-10-26 09:24:09 +0100

    Remove dependency on orphaned bibtex

--- man-roxygen/rox_spcv_coords.R
+++ man-roxygen/rox_spcv_coords.R
@@ -2,6 +2,3 @@
 #'
 #' @description Spatial Cross validation following the "k-means" approach after
 #' Brenning 2012.
-#'
-#' @references
-#' \cite{mlr3spatiotempcv}{brenning2012}
--- man-roxygen/rox_sptcv_cstf.R
+++ man-roxygen/rox_sptcv_cstf.R
@@ -1,6 +1,3 @@
 #' @description Implementation of `CAST::CreateSpaceTimeFolds()`.
 #'
 #' @inherit CAST::CreateSpacetimeFolds details
-#'
-#' @references
-#' \cite{mlr3spatiotempcv}{meyer2018}
