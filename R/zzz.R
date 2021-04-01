48e226a06d77463b787d11b70300f4f734d13e30
pat-s <patrick.schratz@gmail.com>
2021-04-01 14:07:54 +0200

    more mlr3 imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' @rawNamespace import(data.table, except = transpose)
 #' @importFrom R6 R6Class
-#' @importFrom mlr3 TaskClassif TaskRegr Resampling as_data_backend assert_task
+#' @importFrom mlr3 TaskClassif TaskRegr Resampling as_data_backend assert_task rsmp tsk rsmps
 #' @import mlr3misc
 #' @import checkmate
 #' @import paradox
