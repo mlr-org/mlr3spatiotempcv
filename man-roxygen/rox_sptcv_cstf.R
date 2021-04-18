ec59c532608da3da3249443c42e7a6bdce6507db
pat-s <patrick.schratz@gmail.com>
2021-04-18 14:11:25 +0200

    - inherit documentation from upstream functions (#117)

--- man-roxygen/rox_sptcv_cstf.R
+++ man-roxygen/rox_sptcv_cstf.R
@@ -1,3 +1,15 @@
-#' @description Implementation of `CAST::CreateSpaceTimeFolds()`.
+#' @inherit CAST::CreateSpacetimeFolds description details note
 #'
-#' @inherit CAST::CreateSpacetimeFolds details
+#' @note Standard k-fold cross-validation can lead to considerable
+#'   misinterpretation in spatial-temporal modelling tasks. This function can be
+#'   used to prepare a Leave-Location-Out, Leave-Time-Out or
+#'   Leave-Location-and-Time-Out cross-validation as target-oriented validation
+#'   strategies for spatial-temporal prediction tasks. See Meyer et al. (2018)
+#'   for further information.
+#'
+#' @section {mlr3spatiotempcv} notes:
+#'
+#' The 'Description', 'Details' and 'Note' fields are inherited from the
+#' respective upstream function.
+#'
+#' For a list of available arguments, please see [CAST::CreateSpacetimeFold].
