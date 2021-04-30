d27b3c438ac89fe803e26e515f9b2064a189ee98
pat-s <patrick.schratz@gmail.com>
2021-04-30 15:04:53 +0200

    add and update `as_task_classif_st` and `as_task_regr_st`

--- R/TaskClassifST.R
+++ R/TaskClassifST.R
@@ -1,4 +1,4 @@
-#' @title SpatioTemporal Classification Task
+#' @title Crate a Spatiotemporal Classification Task
 #'
 #' @description This task specializes [Task] and [TaskSupervised] for
 #' spatiotemporal classification problems. The target column is assumed to be a
@@ -81,7 +81,7 @@ TaskClassifST = R6::R6Class("TaskClassifST",
         positive = positive, extra_args = extra_args)
 
       self$extra_args$coordinate_names = extra_args$coordinate_names
-      self$extra_args$crs = checkmate::assert_character(extra_args$crs, null.ok = TRUE)
+      # not asserting 'crs' as it can be either chr or int
 
       info = self$col_info[id == target]
       levels = info$levels[[1L]]
--- R/as_task_classif_st.R
+++ R/as_task_classif_st.R
@@ -1,29 +1,45 @@
-#' @title Convert to a SpatioTemporal Classification Task
+#' @title Convert to a Spatiotemporal Classification Task
 #'
 #' @description
-#' Convert object to a [TaskClassifST].
-#' This is a S3 generic, specialized for at least the following objects:
+#' Convertan object to a [TaskClassifST].
+#' This is a S3 generic for the following objects:
 #'
 #' 1. [TaskClassifST]: ensure the identity
-#' 2. [data.frame()] and [DataBackend]: provides an alternative to the constructor of [TaskClassifST].
+#' 2. [data.frame()] and [DataBackend]: provides an alternative to the
+#' constructor of [TaskClassifST].
 #' 2. [sf::sf].
 #'
 #' @inheritParams mlr3::as_task_classif
-#' @param coordinate_names (`character()`)\cr 
-#'   Name of the coordinate columns. Defaults to `c("x", "y")`.
+#' @param  crs `[character(1)]`\cr
+#'     Coordinate reference system. Either a PROJ string or an
+#'     [EPSG](https://epsg.io/) code.
+#' @param coords_as_features `[logical(1)]`\cr
+#'     Whether the coordinates should also be used as features.
+#' @param coordinate_names (`character()`)\cr
+#'     The variables names of the coordinates in the data.
 #'
 #' @return [TaskClassifST].
 #' @export
 #' @examples
+#' library("mlr3")
 #' data("ecuador", package = "mlr3spatiotempcv")
-#' as_task_classif_st(ecuador, target = "slides", positive = "TRUE")
 #'
+#' # data.frame
+#' as_task_classif_st(ecuador, target = "slides", positive = "TRUE",
+#'   coords_as_features = FALSE,
+#'   crs = "+proj=utm +zone=17 +south +datum=WGS84 +units=m +no_defs",
+#'   coordinate_names = c("x", "y"))
+#'
+#' # sf
 #' ecuador_sf = sf::st_as_sf(ecuador, coords = c("x", "y"), crs = 4326)
 #' as_task_classif_st(ecuador_sf, target = "slides", positive = "TRUE")
+#'
+#' # TaskClassifST
+#' task = tsk("ecuador")
+#' as_task_classif_st(task)
 as_task_classif_st = function(x, ...) {
-  UseMethod("as_task_classif_st")
+  UseMethod("as_task_classif_st", x)
 }
-
 
 #' @rdname as_task_classif_st
 #' @export
@@ -31,24 +47,28 @@ as_task_classif_st.TaskClassifST = function(x, clone = FALSE, ...) { # nolint
   if (clone) x$clone() else x
 }
 
-
 #' @rdname as_task_classif_st
 #' @export
-as_task_classif_st.data.frame = function(x, target = NULL, id = deparse(substitute(x)), positive = NULL, coordinate_names = c("x", "y"), ...) { # nolint
+as_task_classif_st.data.frame = function(x, target = NULL, id = deparse(substitute(x)),
+  positive = NULL, crs = NA, coords_as_features = FALSE, coordinate_names = NA, ...) {
   TaskClassifST$new(id = id, backend = x, target = target, positive = positive,
-    extra_args = list(coordinate_names = coordinate_names))
-}
-
-
-#' @rdname as_task_classif_st
-#' @export
-as_task_classif_st.DataBackend = function(x, target = NULL, id = deparse(substitute(x)), positive = NULL, coordinate_names = c("x", "y"), ...) { # nolint
-  TaskClassifST$new(id = id, backend = x, target = target, positive = positive,
-    extra_args = list(coordinate_names = coordinate_names))
+    extra_args = list(coordinate_names = coordinate_names, coords_as_features = coords_as_features,
+      crs = crs))
 }
 
 #' @rdname as_task_classif_st
 #' @export
-as_task_classif_st.sf = function(x, target = NULL, id = deparse(substitute(x)), positive = NULL, ...) { # nolint
-  TaskClassifST$new(id = id, backend = x, target = target, positive = positive)
+as_task_classif_st.DataBackend = function(x, target = NULL, id = deparse(substitute(x)),
+  positive = NULL, crs = NA, coords_as_features = FALSE, coordinate_names = c("x", "y"), ...) {
+  TaskClassifST$new(id = id, backend = x, target = target, positive = positive,
+    extra_args = list(coordinate_names = coordinate_names, coords_as_features = coords_as_features,
+      crs = crs))
+}
+
+#' @rdname as_task_classif_st
+#' @export
+as_task_classif_st.sf = function(x, target = NULL, id = deparse(substitute(x)), positive = NULL,
+  coords_as_features = FALSE, ...) {
+  TaskClassifST$new(id = id, backend = x, target = target, positive = positive,
+    extra_args = list(coords_as_features = coords_as_features))
 }
