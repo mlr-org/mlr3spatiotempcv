3b647faece8c8cc38551126b7b2fb4acd34859b2
pat-s <patrick.schratz@gmail.com>
2021-06-24 09:25:00 +0200

    try fledge in before install

--- tic.R
+++ tic.R
@@ -1,11 +1,13 @@
 do_package_checks(error_on = "warning")
 
+get_stage("before_install") %>%
+  add_step(step_install_github("ropensci/tic@fledge-supp"))
+
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
   get_stage("install") %>%
-    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate")) %>%
-    add_step(step_install_github("ropensci/tic@fledge-supp"))
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
 }
 
