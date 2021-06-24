56f90c9617378f210377695f86288a3fba4c2abf
pat-s <patrick.schratz@gmail.com>
2021-06-24 09:06:27 +0200

    try experimental fledge supp in tic

--- tic.R
+++ tic.R
@@ -4,7 +4,8 @@ if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
   get_stage("install") %>%
-    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate")) %>%
+    add_step(step_install_github("ropensci/tic@fledge-supp"))
   do_pkgdown()
 }
 
