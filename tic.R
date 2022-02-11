04456fa22bad92e458a7aaec1e1c138b1f343eaa
pat-s <patrick.schratz@gmail.com>
2022-02-11 13:24:09 +0100

    remove non-existent tic test branch during install

--- tic.R
+++ tic.R
@@ -1,8 +1,5 @@
 do_package_checks(error_on = "warning")
 
-get_stage("install") %>%
-  add_step(step_install_github("ropensci/tic@fledge-supp"))
-
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
