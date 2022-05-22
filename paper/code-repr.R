7e2263b570a589bac980e4d4cd6e22b0103efc7e
pat-s <patrick.schratz@gmail.com>
2022-05-22 12:27:38 +0200

    paper: various style fixes

--- paper/code-repr.R
+++ paper/code-repr.R
@@ -10,9 +10,9 @@ content = content[6:length(content)]
 # remove all commented lines
 content_no_knitr = content[!grepl("knitr::", content)]
 content_no_comments = content_no_knitr[!grepl("#", content_no_knitr)]
-writeLines(content_no_comments, "paper/code.R")
+writeLines(content_no_comments, "paper/article.R")
 
-source("paper/code.R", chdir = TRUE)
+source("paper/article.R", chdir = TRUE)
 # knitr::spin("paper/code.R")
 # fs::file_move("code.md", "paper/code.md")
 # fs::file_delete(c("code.md", "paper/code.R", "code.html"))
