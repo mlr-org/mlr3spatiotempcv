5879ada8a6c7c896ee4e7a7cee2ae1ebe5ee5745
pat-s <patrick.schratz@gmail.com>
2022-05-26 13:38:21 +0200

    add utils namespace

--- tests/testthat/setup-cluto.R
+++ tests/testthat/setup-cluto.R
@@ -1,14 +1,15 @@
 # install CLUTO executable into $PROJ_ROOT/inst/ for testthat to work
 # system.file() is shimed in testthat and pkgload::load_all()
 if (Sys.getenv("NOT_CRAN") != "") {
+  gist_url = "https://gist.githubusercontent.com/pat-s/6430470cf817050e27d26c43c0e9be72/raw/9a1bee360880be5c2ab8dc5168b4ff9543e499d6/install-cluto.R"
   if (Sys.info()[["sysname"]] == "Linux") {
     if (!file.exists(system.file("vcluster", package = "mlr3spatiotempcv"))) {
-      source("https://gist.githubusercontent.com/pat-s/6430470cf817050e27d26c43c0e9be72/raw/c57cd29c7aeaad55e3f9fca850632a31f4a23280/install-cluto.R")
+      source(gist_url)
     }
   }
   if (Sys.info()[["sysname"]] == "Windows") {
     if (!file.exists(system.file("vcluster.exe", package = "mlr3spatiotempcv"))) {
-      source("https://gist.githubusercontent.com/pat-s/6430470cf817050e27d26c43c0e9be72/raw/fee1895540f25386123bf1fb6e0aca6719c72f53/install-cluto.R")
+      source(gist_url)
     }
   }
 }
