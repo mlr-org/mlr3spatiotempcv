353a842b15ec6a55a498c77ea20556d337aa1b56
pat-s <patrick.schratz@gmail.com>
2022-05-27 13:51:26 +0200

    avoid redundant append of existing col_roles

--- R/zzz.R
+++ R/zzz.R
@@ -63,10 +63,13 @@ register_mlr3 = function() { # nocov start
 
   # append "space" and "time" to col_roles
   # used in CAST
-  x$task_col_roles$classif = c(x$task_col_roles$classif, "space", "time")
-  x$task_col_roles$regr = c(x$task_col_roles$regr, "space", "time")
-  x$task_col_roles$classif_st = c(x$task_col_roles$classif_st, "space", "time")
-  x$task_col_roles$regr_st = c(x$task_col_roles$regr_st, "space", "time")
+  # prevent redudant addition when calling pkgload::load_all()
+  if (!any(c("space", "time") %in% x$task_col_roles$classif)) {
+    x$task_col_roles$classif = c(x$task_col_roles$classif, "space", "time")
+    x$task_col_roles$regr = c(x$task_col_roles$regr, "space", "time")
+    x$task_col_roles$classif_st = c(x$task_col_roles$classif_st, "space", "time")
+    x$task_col_roles$regr_st = c(x$task_col_roles$regr_st, "space", "time")
+  }
 
   # tasks --------------------------------------------------------------------
 
