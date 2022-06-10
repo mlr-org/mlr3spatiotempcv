5c80edc535690068589ad96985b19d5b512e4ae1
pat-s <patrick.schratz@gmail.com>
2022-06-10 08:14:08 +0200

    document helper funs

--- R/helper_DataBackend.R
+++ R/helper_DataBackend.R
@@ -1,9 +1,11 @@
 #' Get CRS from Task or Backend
 #' @description
+#'   Returns the CRS of the given Task or Backend.
 #' @inheritParams mlr3::assert_task
 #' @param pretty `[logical]`\cr
 #'   If `TRUE`, returns the pretty coordinate information instead of raw CRS
 #'   string.
+#' @keywords internal
 get_crs = function(task, pretty = TRUE) {
   if (checkmate::test_class(task$backend, "DataBackendVector")) {
     sf::st_crs(task$backend$geometry)
@@ -14,6 +16,11 @@ get_crs = function(task, pretty = TRUE) {
   }
 }
 
+#' Get coordinates from Task or Backend
+#' @description
+#'   Returns the coordinates of the given Task or Backend.
+#' @inheritParams mlr3::assert_task
+#' @keywords internal
 get_coordinates = function(task) {
   if (checkmate::test_class(task$backend, "DataBackendVector")) {
     as.data.table(sf::st_coordinates(task$backend$geometry))
@@ -24,6 +31,11 @@ get_coordinates = function(task) {
   }
 }
 
+#' Get coordinates names from Task or Backend
+#' @description
+#'   Returns the coordinates names of the given Task or Backend.
+#' @inheritParams mlr3::assert_task
+#' @keywords internal
 get_coordinate_names = function(task) {
   if (checkmate::test_class(task$backend, "DataBackendVector")) {
     names(as.data.table(sf::st_coordinates(task$backend$geometry)))
@@ -34,6 +46,11 @@ get_coordinate_names = function(task) {
   }
 }
 
+#' Check spatial task
+#' @description
+#'   Assertion helper for spatial mlr3 tasks.
+#' @inheritParams mlr3::assert_task
+#' @keywords internal
 assert_spatial_task = function(task) {
   if (!checkmate::test_multi_class(task$backend, c("DataBackendVector", "DataBackendRaster")) &&
     !checkmate::test_multi_class(task, c("TaskClassifST", "TaskRegrST"))) {
