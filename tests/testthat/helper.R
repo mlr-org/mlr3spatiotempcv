f1d5810bcd77596df6459ffbecbb70056346b97e
pat-s <patrick.schratz@gmail.com>
2022-06-11 11:54:26 +0200

    clean

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -170,20 +170,6 @@ test_make_blockCV_test_task = function() {
 
 }
 
-# # subset of cookfarm task for smaller vdiffr snaps
-# # sampling 2 observations per Date+SOURCEID group
-# set.seed(42)
-# # b = as_data_backend(as.data.table(cookfarm_mlr3)[, .SD[sample(x = .N, size = 2)], by = .(Date, SOURCEID)])
-# b = readRDS(test_path("data", "cookfarm_subset.rds"))
-# b$hash = "_mlr3_tasks_cookfarm_"
-# tsk_cookfarm_sub = TaskRegrST$new(
-#   id = "cookfarm", b, target = "PHIHOX",
-#   coordinate_names = c("x", "y"), coords_as_features = FALSE,
-#   crs = 26911
-# )
-# tsk_cookfarm_sub$set_col_roles("Date", roles = "time")
-# tsk_cookfarm_sub$set_col_roles("SOURCEID", roles = "space")
-
 # multiclass tasks -------------------------------------------------------------
 #
 # Create multiclass task
